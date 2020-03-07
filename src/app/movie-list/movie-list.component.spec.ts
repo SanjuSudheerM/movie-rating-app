1624ae09491d0c9b9d879a6b0b6840c31ea88723
Sanju Sudheer M <sanjusdhr@gmail.com>
2020-03-07 18:53:27 +0530

    add random rating functionality

--- src/app/movie-list/movie-list.component.spec.ts
+++ src/app/movie-list/movie-list.component.spec.ts
@@ -1,9 +1,10 @@
-import { async, ComponentFixture, TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, fakeAsync, tick, discardPeriodicTasks } from '@angular/core/testing';
 import { MovieListComponent } from './movie-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Movie } from '../types/types';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { timer } from 'rxjs';
+import { ArrayListPipe } from '../array-list.pipe';
 
 describe('MovieListComponent', () => {
   let component: MovieListComponent;
@@ -131,7 +132,7 @@ describe('MovieListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MovieListComponent],
+      declarations: [MovieListComponent, ArrayListPipe],
       imports: [
         HttpClientModule
       ],
@@ -290,6 +291,8 @@ describe('MovieListComponent', () => {
     const ratingOnUI = elements[elements.length - 1].innerText;
     expect(ratingOnUI).toEqual('8.5');
   });
+
+  it('update existing rating when user updates', () => {
 
     component.movieList = [
       {
@@ -457,5 +460,301 @@ describe('MovieListComponent', () => {
     component.getUpdatedRating(3, 0);
     tick(1000);
     expect(component.movieList[0].rating).toBeGreaterThan(3);
+  }));
+
+  it('should set radom rating enabled true', fakeAsync(() => {
+    component.movieList = [
+      {
+        "movieId": "MOVIE_001",
+        "title": "Joker",
+        "rating": 8.6,
+        "moviePoster": "assets/film-posters/avengers.jpg",
+        "releasedYear": 2019,
+        "category": [
+          "Crime",
+          "Drama",
+          "Thriller"
+        ],
+        "description": "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.",
+        "cast": [
+          {
+            "castName": "Todd Phillips",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNGY5MWRjM2MtNDkzNC00MDcwLWE2ZjYtOWNlZmM3MWIyNGZmXkEyXkFqcGdeQXVyMTAxMjM5NTA4._V1_QL50_SY1000_SX1500_AL_.jpg"
+          },
+          {
+            "castName": "Joaquin Phoenix",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BZGMyY2Q4NTEtMWVkZS00NzcwLTkzNmQtYzBlMWZhZGNhMDhkXkEyXkFqcGdeQXVyNjk1MjYyNTA@._V1_QL50_.jpg"
+          },
+          {
+            "castName": "Robert De Niro",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_QL50_.jpg"
+          }
+        ]
+      },
+      {
+        "movieId": "MOVIE_002",
+        "title": "Avengers: Endgame",
+        "rating": 8.5,
+        "moviePoster": "assets/film-posters/avengers.jpg",
+        "releasedYear": 2019,
+        "category": [
+          "Action",
+          "Adventure",
+          "Drama"
+        ],
+        "description": "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+        "cast": [
+          {
+            "castName": "Anthony Russo",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTc2NjM5MTM0Ml5BMl5BanBnXkFtZTgwMTY3ODczNjM@._V1_QL50_SY1000_CR0,0,635,1000_AL_.jpg"
+          },
+          {
+            "castName": "Joe Russo",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTc2NzY1NTY5OF5BMl5BanBnXkFtZTgwNjY3ODczNjM@._V1_QL50_SY1000_CR0,0,691,1000_AL_.jpg"
+          },
+          {
+            "castName": "Robert Downey Jr.",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNzg1MTUyNDYxOF5BMl5BanBnXkFtZTgwNTQ4MTE2MjE@._V1_QL50_SY1000_CR0,0,664,1000_AL_.jpg"
+          }
+        ]
+      },
+      {
+        "movieId": "MOVIE_003",
+        "title": "The Dark Knight",
+        "rating": 9,
+        "moviePoster": "assets/film-posters/joker.jpg",
+        "releasedYear": 2008,
+        "category": [
+          "Action",
+          "Crime",
+          "Drama"
+        ],
+        "description": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        "cast": [
+          {
+            "castName": "Christopher Nolan",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNjE3NDQyOTYyMV5BMl5BanBnXkFtZTcwODcyODU2Mw@@._V1_UY317_CR7,0,214,317_AL__QL50.jpg"
+          },
+          {
+            "castName": "Christian Bale",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTkxMzk4MjQ4MF5BMl5BanBnXkFtZTcwMzExODQxOA@@._V1_UX214_CR0,0,214,317_AL__QL50.jpg"
+          },
+          {
+            "castName": "Heath Ledger",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTI2NTY0NzA4MF5BMl5BanBnXkFtZTYwMjE1MDE0._V1_UX214_CR0,0,214,317_AL__QL50.jpg"
+          }
+        ]
+      }];
+    component.startRandomRating();
+    discardPeriodicTasks();
+    expect(component.randomRating.isRandomRatingEnabled).toBe(true);
+  }));
+
+  it('should generate random movie index', fakeAsync(() => {
+    component.movieList = [
+      {
+        "movieId": "MOVIE_001",
+        "title": "Joker",
+        "rating": 8.6,
+        "moviePoster": "assets/film-posters/avengers.jpg",
+        "releasedYear": 2019,
+        "category": [
+          "Crime",
+          "Drama",
+          "Thriller"
+        ],
+        "description": "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.",
+        "cast": [
+          {
+            "castName": "Todd Phillips",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNGY5MWRjM2MtNDkzNC00MDcwLWE2ZjYtOWNlZmM3MWIyNGZmXkEyXkFqcGdeQXVyMTAxMjM5NTA4._V1_QL50_SY1000_SX1500_AL_.jpg"
+          },
+          {
+            "castName": "Joaquin Phoenix",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BZGMyY2Q4NTEtMWVkZS00NzcwLTkzNmQtYzBlMWZhZGNhMDhkXkEyXkFqcGdeQXVyNjk1MjYyNTA@._V1_QL50_.jpg"
+          },
+          {
+            "castName": "Robert De Niro",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_QL50_.jpg"
+          }
+        ]
+      },
+      {
+        "movieId": "MOVIE_002",
+        "title": "Avengers: Endgame",
+        "rating": 8.5,
+        "moviePoster": "assets/film-posters/avengers.jpg",
+        "releasedYear": 2019,
+        "category": [
+          "Action",
+          "Adventure",
+          "Drama"
+        ],
+        "description": "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+        "cast": [
+          {
+            "castName": "Anthony Russo",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTc2NjM5MTM0Ml5BMl5BanBnXkFtZTgwMTY3ODczNjM@._V1_QL50_SY1000_CR0,0,635,1000_AL_.jpg"
+          },
+          {
+            "castName": "Joe Russo",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTc2NzY1NTY5OF5BMl5BanBnXkFtZTgwNjY3ODczNjM@._V1_QL50_SY1000_CR0,0,691,1000_AL_.jpg"
+          },
+          {
+            "castName": "Robert Downey Jr.",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNzg1MTUyNDYxOF5BMl5BanBnXkFtZTgwNTQ4MTE2MjE@._V1_QL50_SY1000_CR0,0,664,1000_AL_.jpg"
+          }
+        ]
+      },
+      {
+        "movieId": "MOVIE_003",
+        "title": "The Dark Knight",
+        "rating": 9,
+        "moviePoster": "assets/film-posters/joker.jpg",
+        "releasedYear": 2008,
+        "category": [
+          "Action",
+          "Crime",
+          "Drama"
+        ],
+        "description": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        "cast": [
+          {
+            "castName": "Christopher Nolan",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNjE3NDQyOTYyMV5BMl5BanBnXkFtZTcwODcyODU2Mw@@._V1_UY317_CR7,0,214,317_AL__QL50.jpg"
+          },
+          {
+            "castName": "Christian Bale",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTkxMzk4MjQ4MF5BMl5BanBnXkFtZTcwMzExODQxOA@@._V1_UX214_CR0,0,214,317_AL__QL50.jpg"
+          },
+          {
+            "castName": "Heath Ledger",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTI2NTY0NzA4MF5BMl5BanBnXkFtZTYwMjE1MDE0._V1_UX214_CR0,0,214,317_AL__QL50.jpg"
+          }
+        ]
+      }];
+    component.startRandomRating();
+    tick(component.randomRating.randomInterval);
+    discardPeriodicTasks();
+    expect(component.randomRating.randomMovieIndex).toBeGreaterThan(-1);
+  }));
+
+  it('should enable random rating while clicking on the button', fakeAsync(() => {
+    component.movieList = [
+      {
+        "movieId": "MOVIE_001",
+        "title": "Joker",
+        "rating": 8.6,
+        "moviePoster": "assets/film-posters/avengers.jpg",
+        "releasedYear": 2019,
+        "category": [
+          "Crime",
+          "Drama",
+          "Thriller"
+        ],
+        "description": "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.",
+        "cast": [
+          {
+            "castName": "Todd Phillips",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNGY5MWRjM2MtNDkzNC00MDcwLWE2ZjYtOWNlZmM3MWIyNGZmXkEyXkFqcGdeQXVyMTAxMjM5NTA4._V1_QL50_SY1000_SX1500_AL_.jpg"
+          },
+          {
+            "castName": "Joaquin Phoenix",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BZGMyY2Q4NTEtMWVkZS00NzcwLTkzNmQtYzBlMWZhZGNhMDhkXkEyXkFqcGdeQXVyNjk1MjYyNTA@._V1_QL50_.jpg"
+          },
+          {
+            "castName": "Robert De Niro",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_QL50_.jpg"
+          }
+        ]
+      },
+      {
+        "movieId": "MOVIE_002",
+        "title": "Avengers: Endgame",
+        "rating": 8.5,
+        "moviePoster": "assets/film-posters/avengers.jpg",
+        "releasedYear": 2019,
+        "category": [
+          "Action",
+          "Adventure",
+          "Drama"
+        ],
+        "description": "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+        "cast": [
+          {
+            "castName": "Anthony Russo",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTc2NjM5MTM0Ml5BMl5BanBnXkFtZTgwMTY3ODczNjM@._V1_QL50_SY1000_CR0,0,635,1000_AL_.jpg"
+          },
+          {
+            "castName": "Joe Russo",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTc2NzY1NTY5OF5BMl5BanBnXkFtZTgwNjY3ODczNjM@._V1_QL50_SY1000_CR0,0,691,1000_AL_.jpg"
+          },
+          {
+            "castName": "Robert Downey Jr.",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNzg1MTUyNDYxOF5BMl5BanBnXkFtZTgwNTQ4MTE2MjE@._V1_QL50_SY1000_CR0,0,664,1000_AL_.jpg"
+          }
+        ]
+      },
+      {
+        "movieId": "MOVIE_003",
+        "title": "The Dark Knight",
+        "rating": 9,
+        "moviePoster": "assets/film-posters/joker.jpg",
+        "releasedYear": 2008,
+        "category": [
+          "Action",
+          "Crime",
+          "Drama"
+        ],
+        "description": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        "cast": [
+          {
+            "castName": "Christopher Nolan",
+            "castType": "Director",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BNjE3NDQyOTYyMV5BMl5BanBnXkFtZTcwODcyODU2Mw@@._V1_UY317_CR7,0,214,317_AL__QL50.jpg"
+          },
+          {
+            "castName": "Christian Bale",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTkxMzk4MjQ4MF5BMl5BanBnXkFtZTcwMzExODQxOA@@._V1_UX214_CR0,0,214,317_AL__QL50.jpg"
+          },
+          {
+            "castName": "Heath Ledger",
+            "castType": "Actor",
+            "castImage": "https://m.media-amazon.com/images/M/MV5BMTI2NTY0NzA4MF5BMl5BanBnXkFtZTYwMjE1MDE0._V1_UX214_CR0,0,214,317_AL__QL50.jpg"
+          }
+        ]
+      }];
+    fixture.detectChanges();
+    fixture.debugElement.nativeElement.querySelector('button.button').click();
+    tick(component.randomRating.randomInterval);
+    discardPeriodicTasks();
+    fixture.detectChanges();
+    fixture.debugElement.nativeElement.querySelector()
+    expect(component.randomRating.randomMovieIndex).toBeGreaterThan(-1);
   }));
 });
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Movie } from '../types/types';
+import { Movie, RandomRating } from '../types/types';
 import { environment } from 'src/environments/environment';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-movie-list',
@@ -11,13 +12,14 @@ import { environment } from 'src/environments/environment';
 export class MovieListComponent implements OnInit {
 
   movieList: Array<Movie> = [];
+  randomRating: RandomRating = { isRandomRatingEnabled: false, randomInterval: 0, randomMovieIndex: 0, randomRating: 1 };
+
+  randomTimer: any;
   constructor(public httpService: HttpClient) { }
 
   ngOnInit() {
     this.getMovies();
   }
-
-
   /**
    * sorting the movies in descending order based on the rating
    */
@@ -50,4 +52,30 @@ export class MovieListComponent implements OnInit {
     }, 1000);
   }
 
+
+  startRandomRating() {
+    this.randomRating.isRandomRatingEnabled = true;
+    // generating a random number within 60 and converting to milliseconds
+    // @randomInterval will be in seconds
+    this.randomRating.randomInterval = Math.floor(Math.random() * 60) * 1000;
+
+    this.randomTimer = interval(this.randomRating.randomInterval);
+    this.randomTimer.subscribe(
+      (res: number) => {
+        this.updateRandomMovie();
+      }
+    );
+  }
+
+  updateRandomMovie() {
+    // generating random index of the movie
+    this.randomRating.randomMovieIndex = Math.floor(Math.random() * this.movieList.length);
+    this.randomRating.randomRating = Math.floor(Math.random() * (environment.maximumRating * 10)) / 10;
+    this.movieList[this.randomRating.randomMovieIndex].rating = this.randomRating.randomRating;
+    this.filterBasedOnRating();
+  }
+
+  stopRandomRating() {
+    this.randomTimer.unsubscribe();
+  }
 }
