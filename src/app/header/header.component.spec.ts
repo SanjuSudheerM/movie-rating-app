20da77e722093784598bbf983b59fdd200b0ef95
Sanju Sudheer M <sanjusdhr@gmail.com>
2020-03-07 13:36:27 +0530

    add header component - update typography & variables styles

---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,42 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have as branding \'MOVIE\' in semi bold', () => {
+    const brandingText = `${fixture.debugElement.nativeElement.querySelector('span.title.semi-bold').innerText}`;
+    expect(brandingText).toEqual('MOVIE');
+  });
+
+  it('should have as branding \'RATING\' in thin', () => {
+    const brandingText = `${fixture.debugElement.nativeElement.querySelector('span.title.thin').innerText}`;
+    expect(brandingText).toEqual('RATING');
+  });
+
+  it('should have as branding \'MOVIE RATING\'', () => {
+    // tslint:disable-next-line: max-line-length
+    const brandingText = `${fixture.debugElement.nativeElement.querySelector('span.title.semi-bold').innerText} ${fixture.debugElement.nativeElement.querySelector('span.title.thin').innerText}`;
+    expect(brandingText).toEqual('MOVIE RATING');
+  });
+
+});
